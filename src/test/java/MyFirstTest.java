c217b492b9bdee2c015e90fbf7d7e84af8245230
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-17 12:30:24 -0500

    push to students branch

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+    }
+
+}

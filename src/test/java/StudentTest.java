01cd98041b5f3401da00f1f972fbb65b8cc16f74
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-19 17:15:56 -0500

    update

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,5 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StudentTest {
+
+    Student student;
+
+    @Before
+    public void setUp(){
+        student = new Student("Bob", 1234);
+    }
+
+    @Test
+    public void testNameAndID(){
+        assertEquals(student.getName(), "Bob");
+        assertEquals(1234, 1234);
+    }
+
+    @Test
+    public void testGrades(){
+        assertEquals(0, student.getGrades().size());
+        student.getGrades().add(100);
+        student.getGrades().add(90);
+        assertEquals(2, student.getGrades().size());
+        assertEquals(95, student.getGradeAverage(), .5);
+    }
+
+
 }
